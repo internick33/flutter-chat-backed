47be7d01b52594cb112fe0dcba98ea5510b0b96d
Jose Trinidad Garcia <internick32@gmail.com>
2022-10-20 09:37:46 -0500

    Fin seccion 8

---
+++ helpers/jwt.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+generarJWT = (uid)=> {
+   return new Promise((resolve, reject) => {
+    const payload = {uid};
+    jwt.sign(payload, process.env.JWT_KEY, { expiresIn: '48h' }, (err, token) => {
+        if(err){
+           reject('No se pudo generar el JWT');
+        } else {
+           resolve(token); 
+        }
+    });
+   });
+};
+
+module.exports = {  generarJWT };
